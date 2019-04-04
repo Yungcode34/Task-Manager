e5d87afc844f55aa83b253940915284448faef3b
unknown <andrescardenas34@outlook.com>
2019-04-04 15:03:52 -0700

    working on this

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer id="custom-footer">
+        <span class="mr-4">Andres Cardenas</span>
+        <span class="mr-4">Private</span>
+        <span>Terms of service</span>
+    </footer>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<template>
+<div class="jumbotron custom-bg-dark">
+  <h1 class="display-4">MEVN TASK MANAGER</h1>
+  <p class="lead">This is a simple task manager application build using MongoDb, ExpressJS, VueJS, and NodeJS</p>
+  <hr class="my-4">
+  <p>Click below to manage task for users</p>
+  <a class="btn btn-success btn-lg" href="#" role="button">View Task</a>
+</div>
+</template>
